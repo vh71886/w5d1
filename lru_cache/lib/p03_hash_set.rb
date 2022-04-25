8c28a356a0cdc1444de1250eb1198ea926e32646
Vera Ho <vh71886@gmail.com>
2022-04-25 12:12:53 -0400

    Completed HashSet

--- lru_cache/lib/p03_hash_set.rb
+++ lru_cache/lib/p03_hash_set.rb
@@ -7,18 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    if count > num_buckets - 1
+      resize!
+    end
+
+    if !include?(key)
+      self[key.hash] << key 
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key.hash].each { |n| return true if n == key }
+    false
   end
 
   def remove(key)
+    return if !include?(key)
+    self[key.hash].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +39,15 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(@store.length * 2) { Array.new }
+    @count = 0
+
+    old_store.each do |bucket|
+      bucket.each do |key|
+        insert(key)
+      end
+    end
   end
+
 end
