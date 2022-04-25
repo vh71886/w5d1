ed1de70c5215d844c810923fa1738a10df97dd12
Vera Ho <vh71886@gmail.com>
2022-04-25 11:49:47 -0400

    Completed Array#hash and String#hash

--- lru_cache/lib/p02_hashing.rb
+++ lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    return 0.hash if self.empty?
+    self[1..-1].inject(self.first) do |accul, ele|
+      (accul + ele).hash
+    end
   end
 end
 
 class String
   def hash
+    num = self.to_i(36)
+    num.hash
   end
 end
 
@@ -16,6 +22,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+
+    
     0
   end
 end
