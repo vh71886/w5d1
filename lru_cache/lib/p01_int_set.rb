013b40c44493b468dfa7edae3bd072d35824a956
Vera Ho <vh71886@gmail.com>
2022-04-25 10:34:07 -0400

    Completed MaxIntSet and IntSet

--- lru_cache/lib/p01_int_set.rb
+++ lru_cache/lib/p01_int_set.rb
@@ -1,14 +1,22 @@
 class MaxIntSet
+  attr_accessor :store
+  
   def initialize(max)
+    @max = max
+    @store = Array.new(@max, false) # init to max length, empty array
   end
 
   def insert(num)
+    raise "Out of bounds" if num > @max || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
@@ -27,18 +35,25 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num if !include?(num)
   end
 
   def remove(num)
+    return if !include?(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].each { |n| return true if n == num }
+    false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    # usage: self[num] == @store[num % num_buckets]
+    @store[num % num_buckets]
   end
 
   def num_buckets
